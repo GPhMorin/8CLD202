5b97a0341dab427eb1c06ec8b732ac1cb42ae7cd
Zowlak <76110476+Zowlak@users.noreply.github.com>
2023-11-30 09:43:21 -0500

    Update p2p.sh

--- p2p.sh
+++ p2p.sh
@@ -4,11 +4,11 @@
 # Set the number of threads to 1
 export OMP_NUM_THREADS=1
 
-# If you're on mac, uncomment next line and replace ./venv/bin/activate by : /venv/bin/activate
+# If you're on mac, uncomment next line and replace ./venv/bin/activate by : venv/bin/activate
 # python3 -m venv venv
 # Set the Python virtual environment
 source ./venv/bin/activate
-# Install the requirements
+# Install requirements
 pip3 install -r requirements.txt
 
 # Run torchrun with the provided number of processes
