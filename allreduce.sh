6a2f03b44bd25c4a45166d5c6ccc521d7eb3277b
Zowlak <76110476+Zowlak@users.noreply.github.com>
2023-11-30 08:36:48 -0500

    Update allreduce.sh
    Adding Mac Support & requirements installation in the venv

--- allreduce.sh
+++ allreduce.sh
@@ -4,8 +4,12 @@
 # Set the number of threads to 1
 export OMP_NUM_THREADS=1
 
+# If you're on mac, uncomment next line and replace ./venv/bin/activate by : /venv/bin/activate
+# python3 -m venv venv
 # Set the Python virtual environment
 source ./venv/bin/activate
+# Install requirements
+pip3 install -r requirements.txt
 
 # Run torchrun with the provided number of processes
 torchrun --nproc-per-node $1 allreduce.py
